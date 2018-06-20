36953798e7f5e446c4d1e6c1b8dd107b5cb36903
ykoh <kenshin579@gmail.com>
2018-06-20 21:01:09 +0900

    - 불필요한 dependency 제거함

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 var express = require('express');
-var logger = require('../config/logger.js');
-var moment = require('moment');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Timer = mongoose.model('timer');
 
 // return a list of timers
 router.get('/', function (req, res, next) {
